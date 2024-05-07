b79657d6d62e8a77f0ef00d2ab83591087430bd8
Bruno-Pierre <zerod.zunaro@gmail.com>
2024-05-06 22:11:00 -0400

    feat: updated menu [LeoninQc]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
-export default function Home() {
+'use client'
+
+import { MenuNavigation } from '@/components'
+import { menuRoutes } from '@/definition'
+import { Button, Drawer } from '@mui/material'
+import { useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu'
+
+export default function AppHome() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleMenu = (newMenuState: boolean) => {
+    setMenuOpen(newMenuState)
+  }
+
   return (
-    <div id="contenu" className="container mx-auto">
-      <div className="grid grid-cols-12 gap-4">
-        <div className="border border-solid border-red-700 col-span-9">Hello</div>
-        <div className="border border-solid border-blue-700 col-span-3">World</div>
+    <>
+      <Drawer open={menuOpen} onClose={() => handleMenu(false)}>
+        <MenuNavigation menuItems={menuRoutes} />
+      </Drawer>
+      <div id="contenu" className="container mx-auto">
+        <Button onClick={() => handleMenu(true)}>
+          <MenuIcon />
+        </Button>
+        <div className="grid grid-cols-12 gap-4">
+          <div className="border border-solid border-red-700 col-span-9">Hello</div>
+          <div className="border border-solid border-blue-700 col-span-3">World</div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
